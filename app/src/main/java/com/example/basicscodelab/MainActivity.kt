542d95d42b68c84cf2f7677c645ba2fedac9bcab
Anthony <ovandoa26@gmail.com>
2024-01-07 08:38:19 -0500

    button + columns + rows

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,22 +3,23 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.basicscodelab.ui.theme.BasicsCodeLabTheme
 
 
@@ -34,51 +35,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(modifier: Modifier = Modifier) {
-    // A surface container using the 'background' color from the theme
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
+        // with elevated button, it inverts
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp),
     ) {
-        GreetingText(message = "Happy Birthday Anthony!", from = "From Emma")
+        Row {
+            Column(
+                modifier = Modifier.padding(24.dp).weight(1f, true)
+            ) {
+                Text(text = "Hello")
+                Text(text = name)
+            }
+                ElevatedButton(
+                    modifier = Modifier.align(Alignment.CenterVertically).padding(end = 24.dp),
+                    onClick = { } //
+
+                ) {
+                    Text("Show more")
+                }
+        }
     }
 }
 
-
-@Composable
-fun GreetingText(modifier: Modifier = Modifier, message: String, from: String) {
-    Column (
-        modifier = modifier,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = message,
-            fontSize = 50.sp,
-            lineHeight = 54.sp,
-            textAlign = TextAlign.Center,
-            color = Color.Blue,
-            modifier = Modifier.
-            padding(horizontal = 24.dp,  vertical = 8.dp)
-        )
-
-        Text(
-            text = from,
-            fontSize = 9.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.End)
-        )
-    }
-}
-
-
-// annotation can takes parameters, preview won't be reflected in app
 @Preview(
-    showBackground = true
-)
+    showBackground = true,
+    widthDp = 320,
+    )
 @Composable
-fun BirthdayCardPreview() {
+fun GreetingPreview() {
     BasicsCodeLabTheme {
-        GreetingText(message = "Happy Birthday Anthony!", from = "From Emma")
+        MyApp()
     }
 }
